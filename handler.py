92530527360d42fae1315acb970aa39e051d37cd
yochaibl <yochaib@protonmail.com>
2020-03-07 16:06:01 +0000

    dir

--- handler.py
+++ handler.py
@@ -14,7 +14,7 @@ def hello(params):
   #    body = run('is') # todo
   #except Exception as exc:
   #    body = 'todo error: ' + str(exc)
-  body = params.get('bla', 'bladefault') # todo
+  body = dir(params) # params.get('bla', 'bladefault') # todo
 
   response = {
       "statusCode": 200,
