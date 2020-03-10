b1b16abdc7174f5f9ea8651697a9e8d05abd1dbf
LucyCo <lucy.co1608@gmail.com>
2020-03-10 21:20:15 +0200

    Update handler.py

--- handler.py
+++ handler.py
@@ -12,18 +12,28 @@ TIMEOUT_SECONDS = 25
 GET_QUERY = event.get('queryStringParameters', {}).get('phrase', '').lower()
 
 HACKER_NEWS_PREFIX = "https://hacker-news.firebaseio.com/v0/"
-
 HACKER_NEWS_ITEM_PATH = "item/"
-
-TOP_STORIES = "topstories"
+HACKER_NEWS_TOP_STORIES = "topstories"
+HACKER_NEWS_TITLE = "title"
+HACKER_NEWS_KIDS = "kids"
+HACKER_NEWS_TEXT = "text"
 
 JSON_SUFFIX = ".json"
 
-TITLE = "title"
+SENTIMENT_POS = "positive"
+SENTIMENT_NEG = "negative"
+SENTIMENT_NEU = "neutral"
+SENTIMENT_MIX = "mixed"
 
-KIDS = "kids"
+VADER_POS = "pos"
+VADER_NEG = "neg"
+VADER_NEU = "neu"
+VADER_MIX = "compound"
 
-TEXT = "text"
+STATS_COMMENTS = "comments"
+STATS_AVG = "avg"
+STATS_MED = "median"
+
 
 timestamp = datetime.timestamp(datetime.now())
 
@@ -31,7 +41,7 @@ loop = asyncio.get_event_loop()
 
 analyzer = SentimentIntensityAnalyzer()
 
-stats = {"positive": [], "negative": [], "neutral": [], "mixed": []}
+stats = {SENTIMENT_POS: [], SENTIMENT_NEG: [], SENTIMENT_NEU: [], SENTIMENT_MIX: []}
 
 allStoryUrls = []
 
@@ -88,7 +98,7 @@ def sentiment(event, context):
 
 def run(phrase):
     urls = []
-    urls.append(HACKER_NEWS_PREFIX + TOP_STORIES + JSON_SUFFIX)
+    urls.append(HACKER_NEWS_PREFIX + HACKER_NEWS_TOP_STORIES + JSON_SUFFIX)
     results = loop.run_until_complete(fetch_all(urls))
     story_id_list = results[0]
 
@@ -101,19 +111,19 @@ def run(phrase):
     all_direct_story_kids_id = [];
 
     for story in results:
-        if story.get(TITLE).lower().find(phrase) != -1 and story.get(KIDS) is not None:
-            for comment_id in story.get(KIDS, []):
+        if story.get(HACKER_NEWS_TITLE).lower().find(phrase) != -1 and story.get(HACKER_NEWS_KIDS) is not None:
+            for comment_id in story.get(HACKER_NEWS_KIDS, []):
                 all_direct_story_kids_id.append(comment_id)
 
     get_comments(all_direct_story_kids_id)
 
-    sum = len(stats["positive"])
-    output = {"comments":sum}
+    sum = len(stats[SENTIMENT_POS])
+    output = {STATS_COMMENTS:sum}
     if sum != 0:
         for attr in stats:
             dict = {
-            'avg':"%.3f" % mean(stats[attr]),
-            'median':"%.3f" % median(stats[attr])
+            STATS_AVG:"%.3f" % mean(stats[attr]),
+            STATS_MED:"%.3f" % median(stats[attr])
             }
             output[attr] = dict
     return output
@@ -123,10 +133,10 @@ def update_sentiments(text):
     if not text:
         return
     result = analyzer.polarity_scores(text)
-    stats["positive"].append(result["pos"])
-    stats["negative"].append(result["neg"])
-    stats["neutral"].append(result["neu"])
-    stats["mixed"].append(result["compound"])
+    stats[SENTIMENT_POS].append(result[VADER_POS])
+    stats[SENTIMENT_NEG].append(result[VADER_NEG])
+    stats[SENTIMENT_NEU].append(result[VADER_NEU])
+    stats[SENTIMENT_MIX].append(result[VADER_MIX])
 
 def get_url_from_id(id):
     return HACKER_NEWS_PREFIX + HACKER_NEWS_ITEM_PATH + str(id) + JSON_SUFFIX
@@ -137,8 +147,8 @@ def get_comments(comment_ids):
         return
     comment_urls = [];
     for comment_id in comment_ids:
-        comment_urls.append(get_url_from_id(commentId))
+        comment_urls.append(get_url_from_id(comment_id))
     result = loop.run_until_complete(fetch_all(comment_urls))
     for comment in result:
-        update_sentiments(comment.get(TEXT))
-        get_comments(comment.get(KIDS, []))
+        update_sentiments(comment.get(HACKER_NEWS_TEXT))
+        get_comments(comment.get(HACKER_NEWS_KIDS, []))
